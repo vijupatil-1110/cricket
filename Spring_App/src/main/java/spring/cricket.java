1d7e357f2d1ab91eb6969731ffb7e67445cb1565
om <https://github.com/vijupatil-1110/voting-app.git>
2020-05-15 10:58:54 +0530

    First commit

---
+++ Spring_App/src/main/java/spring/cricket.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class cricket {
+	 @RequestMapping("/")
+	    String home() {
+	        return "Spring Boot Application";
+	    }
+}

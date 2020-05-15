c119fd405c8ff24b97fb382b53afeca12040d32b
vijupatil-1110 <65228292+vijupatil-1110@users.noreply.github.com>
2020-05-15 18:31:04 +0530

    Update cricket.java

--- Spring_App/src/main/java/spring/cricket.java
+++ Spring_App/src/main/java/spring/cricket.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class cricket {
 	 @RequestMapping("/")
 	    String home() {
-	        return "Vijaykumar! How are you ?";
+	        return "Vijaykumar Patil! How are you ?";
 	    }
 }
